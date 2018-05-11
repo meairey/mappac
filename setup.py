b75bb72237b523e221202e812c2c52fa6497238c
meairey <mea2194@columbia.edu>
2018-05-10 21:28:08 -0400

    added the setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+# parse version string from the __init__ file
+with open("Project/__init__.py", "r") as initfile:
+    lines = initfile.readlines()
+    for line in lines:
+        if "__version__" in line:
+            # get version line and strip white space and quotations
+            version = line.strip().split()[-1].strip("'").strip('"')
+
+
+# build command
+setup(
+    name="mappac",
+    version=version,
+    packages=find_packages(),
+    author="Montana Airey",
+    author_email="mea2194@columbia.edu",
+    license="GPLv3",
+    description="A package for creating contour maps of ocean currents",
+    classifiers=["Programming Language :: Python :: 3"],
+)
